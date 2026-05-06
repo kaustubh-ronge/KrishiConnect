ede71365999e43e855359afcb245c5dd17ff8bf2
kaustubh-ronge <kaustubhronge25@gmail.com>
2026-05-06 21:46:35 +0530

    resolved continous route loop

--- app/(client)/onboarding/page.jsx
+++ app/(client)/onboarding/page.jsx
@@ -1,11 +1,11 @@
 
 export const dynamic = 'force-dynamic';
-import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation"; // ADD THIS
 import OnboardingClient from "./_components/OnboardinClient";
+import { checkUser } from "@/lib/checkUser";
 
 export default async function OnboardingPage() {
-  const { sessionClaims } = await auth();
+  const { sessionClaims } = await checkUser();
   const role = sessionClaims?.role;
 
   // Onboarding page (server): role fetched
